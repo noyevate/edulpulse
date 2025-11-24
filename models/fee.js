4a2055abb1508a576ef182e8a555ad8cb059b036
Samuel Lucky <samuelnoye35@gmail.com>
2025-11-24 04:59:13 +0100

    Update fee.js

--- models/fee.js
+++ models/fee.js
@@ -5,7 +5,7 @@ const FeeDetailsSchema = new mongoose.Schema(
     gradeLevel: {
       type: String,
       enum: [
-        '100', '200', '300', '400', '500', '600',
+        '100L', '200L', '300L', '400L', '500L', '600L',
       ],
       required: true
     },
